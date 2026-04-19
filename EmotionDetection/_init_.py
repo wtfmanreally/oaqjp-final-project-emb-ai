f512650b36f5e2ec40c10bffff3428fa2bb6d222
wtfmanreally <ryker.holt@yahoo.com>
2026-04-19 09:05:50 -0700

    Update _init_.py

--- EmotionDetection/_init_.py
+++ EmotionDetection/_init_.py
@@ -1,1 +1,10 @@
 from . import emotion_detection
+"""
+EmotionDetection Package
+
+Provides emotion detection functionality using IBM Watson NLP API.
+"""
+
+from .emotion_detection import emotion_detector
+
+__all__ = ['emotion_detector']
